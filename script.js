1a50c9a36e93a81062b005c226799666ef12e0cc
Chris Black <black@chrisblack.net>
2020-07-25 11:38:35 -0400

    Node generation in progess.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ configInput.addEventListener("input", tasteTheRainbow);
 function tasteTheRainbow(event) {
     const colors = extractColors(event);
     console.log(colors);
+    listColors(colors);
 }
 
 function extractColors(event) {
@@ -21,4 +22,29 @@ function extractColors(event) {
     const configObject = JSON.parse(configObjectJSON);
     const colors = configObject.theme.colors;
     return colors;
+}
+
+function listColors(colors) {
+    const colorList = document.getElementById("color-list");
+    const colorEntries = Object.entries(colors);
+    colorEntries.forEach(([colorName, value]) => {
+        const hasModifiers = Object.keys(value).length > 0;
+        // Create node for color
+        const colorNameDiv = document.createElement("div");
+        let content = `<h3>${colorName}${!hasModifiers ? " {}" : ""}</h3>`;
+        colorNameDiv.innerHTML = content;
+        colorList.appendChild(colorNameDiv);
+        if (hasModifiers) {
+            listModifiers(colorName, colorNameDiv, value);
+        }
+    });
+}
+
+function listModifiers(colorName, element, modifiers) {
+    const modifierEntries = Object.entries(modifiers);
+    modifierEntries.forEach(([modifier, value]) => {
+        const className = `${colorName}-${modifier}`;
+        console.log(className);
+        // Create node with className and color hex value, with style of hex valueg
+    });
 }
