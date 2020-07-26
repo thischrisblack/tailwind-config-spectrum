60a68b1e7e7cd5a2c0f39cfc2be8c12037da4f63
Chris Black <black@chrisblack.net>
2020-07-26 15:40:18 -0400

    Commented and cleaned up.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tailwind Config Color Spectrum
 
-If you use Tailwind CSS and you have a lot of custom colors in your <code>tailwind.config.js</code> file, it can get confusing. With this tool, you can paste the contents of your entire config file into a textarea and see what colors and color classes you're using. Click on a color bar to copy its class suffix to your clipboard.
+If you use Tailwind CSS and you have a lot of custom colors in your <code>tailwind.config.js</code> file, it can get confusing. With this tool, you can paste the contents of your entire config file into a textarea and see what colors and color classes you're using. This will help avoid duplicates or near duplicates, and also help you pick a good custom suffix to place a new color properly within the light-to-dark gradient. You can click on a color bar to copy its class suffix to your clipboard.
 
-You can use it [here](https://thischrisblack.github.io/tailwind-config-spectrum/).
+**You can use it [here](https://thischrisblack.github.io/tailwind-config-spectrum/).**
 
 ![Screenshot](./src/tailwind-screenshot.png)
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ configInput.addEventListener("input", tasteTheRainbow);
 function tasteTheRainbow(event) {
     const colors = extractColors(event);
     if (!colors) {
+        // Invalid input.
         notValid.classList.remove("hidden");
         return;
     }
@@ -19,49 +20,64 @@ function tasteTheRainbow(event) {
     inputArea.classList.add("hidden");
 }
 
+/**
+ * Parse the pasted string and return as an object
+ * @param {*} event The input update event
+ */
 function extractColors(event) {
     const input = event.target.value;
     // Deal with the pasted-in string, accounting for differences in format
     const firstBrace = input.indexOf("{");
     const lastBrace = input.lastIndexOf("}");
     const configObjectJSON = input
+        // Just get the object
         .substring(firstBrace, lastBrace + 1)
+        // Get rid of newlines
         .split("\n")
         .join("")
+        // Get rid of spaces
         .replace(/\s/g, "")
+        // Change single quotes to double quotes
         .replace(/'/g, '"')
+        // Anything that was "'Some String'" is now ""Some String"" so fix that
         .replace(/\"\"/g, '"')
+        // Put all the keys in quotes for valid JSON
         .replace(/([{,])(\s*)([A-Za-z0-9_\-]+?)\s*:/g, '$1"$3":')
+        // Remove final commas
         .replace(/},}/g, "}}")
         .replace(/\",}/g, `"}`);
-    // Get just the "colors" part.
+
+    // Get just the "colors" part of the config.
     let justTheColors = configObjectJSON.match(/({|,)\"colors\":(.*?)}},/);
 
     if (!justTheColors) {
+        // It was invalid input.
         return null;
     }
 
-    // Remove the trailing comma and add outer brackets
+    // Remove the trailing comma and add outer braces
     justTheColors = `{${justTheColors[0].slice(
         1,
         justTheColors[0].length - 1
     )}}`;
 
+    // Parse that string, finally, and get the colors prop only.
     const colors = JSON.parse(justTheColors).colors;
 
     return colors;
 }
 
+/**
+ * Builds the list of top-level colors in the UI, and then adds the modifiers
+ * @param {*} colors The colors prop of the config file, parsed as an object
+ */
 function listColors(colors) {
     const colorEntries = Object.entries(colors);
 
     colorEntries.forEach(([colorName, value]) => {
         const hasModifiers = Object.keys(value).length > 0;
-
-        const colorNameDiv = document.createElement("div");
         let content = `<h3>${colorName}${!hasModifiers ? " {}" : ""}</h3>`;
-        colorNameDiv.innerHTML = content;
-        colorNameDiv.classList.add("color-block");
+        const colorNameDiv = createNode("div", content, "color-block");
         colorContainer.appendChild(colorNameDiv);
 
         if (hasModifiers) {
@@ -70,6 +86,12 @@ function listColors(colors) {
     });
 }
 
+/**
+ * Renders the list of class suffixes within each color block
+ * @param {*} colorName The name of the color (i.e. "red")
+ * @param {*} parentElement The parent color block element
+ * @param {*} modifiers The array of class suffixes (i.e. "100", "200", etc.)
+ */
 function listModifiers(colorName, parentElement, modifiers) {
     if (typeof modifiers !== "string") {
         const modifierEntries = Object.entries(modifiers);
@@ -83,6 +105,12 @@ function listModifiers(colorName, parentElement, modifiers) {
     }
 }
 
+/**
+ * Renders the colored bars with the class suffixes and hex values in the UI
+ * @param {*} colorName The name of the color
+ * @param {*} modifier The class suffix
+ * @param {*} value The hex color
+ */
 function createColorBar(colorName, modifier, value) {
     const className = `${colorName}${modifier ? "-" + modifier : ""}`;
 
@@ -92,12 +120,19 @@ function createColorBar(colorName, modifier, value) {
 
     const classNameElement = createNode("span", className, "color-bar-text");
     const hexNameElement = createNode("span", value, "color-bar-text");
+
     colorBar.appendChild(classNameElement);
     colorBar.appendChild(hexNameElement);
 
     return colorBar;
 }
 
+/**
+ * Node-generating util function
+ * @param {*} nodeType Type of node
+ * @param {*} content innerHTML value of the node
+ * @param {*} className Class to add
+ */
 function createNode(nodeType, content, className) {
     const node = document.createElement(nodeType);
     node.innerHTML = content;
@@ -105,6 +140,9 @@ function createNode(nodeType, content, className) {
     return node;
 }
 
+/**
+ * Adds click listeners to each color bar
+ */
 function addListeners() {
     const colorBars = document.querySelectorAll(".color-bar");
     colorBars.forEach((colorBar) =>
@@ -112,24 +150,35 @@ function addListeners() {
     );
 }
 
+/**
+ * Copies the class suffix to the user clipboard
+ * @param {*} event The click event
+ */
 function copyClass(event) {
+    // Create hidden input element with the text to copy as the value
     const hiddenInput = document.createElement("input");
     hiddenInput.value = `-${event.target.id}`;
     hiddenInput.style.opacity = 0;
     event.target.appendChild(hiddenInput);
+    // Select the tet in the element
     hiddenInput.select();
+    // Copy it
     document.execCommand("copy");
+    // Remove it
     event.target.removeChild(hiddenInput);
     showCopiedMessage(event.target);
 }
 
+/**
+ * Shows a message to the user on click/copy
+ * @param {*} element The clicked element
+ */
 function showCopiedMessage(element) {
     const parentElement = event.target;
-    const span = document.createElement("span");
-    span.classList.add("copied-message");
-    span.textContent = `Class suffix "-${parentElement.id}" copied to clipboard`;
+    const content = `Class suffix "-${parentElement.id}" copied to clipboard`;
+    const span = createNode("span", content, "copied-message");
     parentElement.appendChild(span);
     setTimeout(() => {
         parentElement.removeChild(span);
-    }, 2000);
+    }, 3000);
 }
